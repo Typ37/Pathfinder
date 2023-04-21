18a15f70cc42dfcfb6c645ce7dc2e1f7ae9ca384
Typ37 <aidenldonavan@gmail.com>
2023-04-21 13:33:09 -0700

    made the buttons look how i want them to loook

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ master=tkinter.Tk()
 master.title("grid() method")
 master.geometry("350x275")
 
-button1=tkinter.Button(master, text="button1")
-button1.grid(row=1,column=0)
+count = 1
 
-button2=tkinter.Button(master, text="button2")
-button2.grid(row=2,column=2)
+for x in range (15):
+    for y in range(15):
+        button=tkinter.Button(master, text = "     ")
+        button.grid(row = x,column = y)
 
-button3=tkinter.Button(master, text="button3")
-button3.grid(row=3,column=3)
+        #button2=tkinter.Button(master, text="button2")
+        #button2.grid(row= x,column= y)
 
-button4=tkinter.Button(master, text="button4")
-button4.grid(row=4,column=4)
+        #button3=tkinter.Button(master, text="button3")
+        #button3.grid(row= x,column= y)
 
-button5=tkinter.Button(master, text="button5")
-button5.grid(row=5,column=5)
+        #button4=tkinter.Button(master, text="button4")
+        #button4.grid(row= x,column= y)
+
+        #button5=tkinter.Button(master, text="button5")
+        #button5.grid(row= x,column= y)
+        count = count + 1
 
 master.mainloop()
--- toggle.py
+++ toggle.py
@@ -1,3 +1,5 @@
+import sys
+print(sys.executable)
 from tkinter import *
 from PIL import Image, ImageTk
 import tkinter
