671c6a1e4ed0e5ef043bf27875e02a3440cc2707
Typ37 <aidenldonavan@gmail.com>
2023-04-24 13:58:32 -0700

    GRAHH GRAH GRAHHH EURGHHH CHIEEECK

--- main.py
+++ main.py
@@ -11,18 +11,6 @@ for x in range (15):
     for y in range(15):
         button=tkinter.Button(master, text = "     ")
         button.grid(row = x,column = y)
-
-        #button2=tkinter.Button(master, text="button2")
-        #button2.grid(row= x,column= y)
-
-        #button3=tkinter.Button(master, text="button3")
-        #button3.grid(row= x,column= y)
-
-        #button4=tkinter.Button(master, text="button4")
-        #button4.grid(row= x,column= y)
-
-        #button5=tkinter.Button(master, text="button5")
-        #button5.grid(row= x,column= y)
         count = count + 1
 
 master.mainloop()
--- toggle.py
+++ toggle.py
@@ -1,52 +1,44 @@
-import tkinter
+# Import tkinter in the notebook
 from tkinter import *
-from PIL import Image, ImageTk
 
-# Create Object
-root = Tk()
- 
-# Add Title
-root.title('On/Off Switch!')
- 
-# Add Geometry
-root.geometry("500x300")
- 
-# Keep track of the button state on/off
-#global is_on
+# Create an instance of window of frame
+win = Tk()
+
+# set Title
+win.title('Toggle Button Demonstration')
+
+# Set the Geometry
+win.geometry("700x400")
+win.resizable(0, 0)
+
+# Create a variable to turn on the button initially
 is_on = True
- 
-# Create Label
-my_label = Label(root,
-    text = "The Switch Is On!",
-    fg = "green",
-    font = ("Helvetica", 32))
- 
-my_label.pack(pady = 20)
- 
+
+# Create Label to display the message
+label = Label(win, text="On", bg="white", fg="black", font=("Poppins bold", 22))
+label.pack(pady=20)
+
 # Define our switch function
-def switch():
-    global is_on
-     
-    # Determine is on or off
-    if is_on:
-        on_button.config(image = off)
-        my_label.config(text = "The Switch is Off",
-                        fg = "grey")
-        is_on = False
-    else:
-       
-        on_button.config(image = on)
-        my_label.config(text = "The Switch is On", fg = "green")
-        is_on = True
- 
+def button_mode():
+   global is_on
+
+   # Determine it is on or off
+   if is_on:
+      on_.config(image=off)
+      label.config(text="Off", bg="white", fg="black")
+      is_on = False
+   else:
+      on_.config(image=on)
+      label.config(text="On", fg="black")
+      is_on = True
+
 # Define Our Images
-on = PhotoImage(file = "on.png")
-off = PhotoImage(file = "off.png")
- 
+on = PhotoImage(file="on.png")
+off = PhotoImage(file="off.png")
+
 # Create A Button
-on_button = Button(root, image = on, bd = 0,
-                   command = switch)
-on_button.pack(pady = 50)
- 
-# Execute Tkinter
-root.mainloop()
+on_ = Button(win, image=on, bd=0, command=button_mode)
+on_.pack(pady=25)
+
+# Keep Running the window
+win.mainloop()
